07115c73a138e7a8b0337905714f18782876ea74
NelizaTaconi <neliza.taconi@afr.org.br>
2022-08-01 12:41:08 -0300

    Ajustes

--- aluno/urls.py
+++ aluno/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 
 from aluno.models import Aluno
 from .views import cadastra_aluno, lista_aluno
+
+app_name = 'aluno'
+
 urlpatterns = [
     path('cadastra/', cadastra_aluno, name='cadastra_aluno'),
-    path('lista/', lista_aluno, name = 'lista_aluno')
+    path('lista/', lista_aluno, name='lista_aluno')
 
 ]
