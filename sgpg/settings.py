0ce1597ee52ab2ca924d837675d4c392ab9c025c
lucasamasan <120474427+lucasamasan@users.noreply.github.com>
2022-12-13 14:27:03 -0300

    Update settings.py

--- sgpg/settings.py
+++ sgpg/settings.py
@@ -160,12 +160,12 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), ]
+elif DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+    
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
