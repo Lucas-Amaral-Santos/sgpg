c040f679de7960cc0372ae8cf125c93bfce11c03
lucas <amarallucas@id.uff.br>
2024-11-16 15:16:04 -0300

    Atualizar relatório

--- relatorios/filter.py
+++ relatorios/filter.py
@@ -2,7 +2,7 @@ import django_filters
 from django import forms
 from aluno.models import Aluno
 from matricula.models import Matricula, Probatorio
-from config.models import Sexo, EstadoCivil, Etnia, StatusOptions
+from config.models import Sexo, EstadoCivil, Etnia, StatusOptions, Grau
 
 class AlunoFilter(django_filters.FilterSet):
     nome = django_filters.CharFilter(lookup_expr='icontains')
@@ -10,6 +10,17 @@ class AlunoFilter(django_filters.FilterSet):
     estado_civil = django_filters.MultipleChoiceFilter(choices=[], widget=forms.CheckboxSelectMultiple, label="Estado Civil:")
     etnia = django_filters.MultipleChoiceFilter(choices=[], widget=forms.CheckboxSelectMultiple, label="Etnia:")    
     status__status = django_filters.MultipleChoiceFilter(choices=[], widget=forms.CheckboxSelectMultiple, label="Status:")
+    probatorio_aluno__grau = django_filters.MultipleChoiceFilter(choices=[], widget=forms.CheckboxSelectMultiple, label="Grau:")
+    
+    
+    dt_nascimento = django_filters.NumberFilter(field_name='dt_nascimento', lookup_expr='year', label="Data de Nascimento:")
+    dt_nascimento__year__gt = django_filters.NumberFilter(field_name='dt_nascimento', lookup_expr='year__gt', label="Data de Nascimento - Acima de:")
+    dt_nascimento__year__lt = django_filters.NumberFilter(field_name='dt_nascimento', lookup_expr='year__lt', label="Data de Nascimento - Abaixo de:")
+    
+    dt_cadastro = django_filters.NumberFilter(field_name='dt_cadastro', lookup_expr='year', label="Data de Cadastro:")
+    dt_cadastro__year__gt = django_filters.NumberFilter(field_name='dt_cadastro', lookup_expr='year__gt', label="Data de cadastro - Acima de:")
+    dt_cadastro__year__lt = django_filters.NumberFilter(field_name='dt_cadastro', lookup_expr='year__lt', label="Data de cadastro - Abaixo de:")
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,10 +28,11 @@ class AlunoFilter(django_filters.FilterSet):
         self.filters['estado_civil'].extra['choices'] = [(estado_civil.id, estado_civil) for estado_civil in EstadoCivil.objects.all()]
         self.filters['etnia'].extra['choices'] = [(etnia.id, etnia) for etnia in Etnia.objects.all()]
         self.filters['status__status'].extra['choices'] = [(status.id, status) for status in StatusOptions.objects.all()]
+        self.filters['probatorio_aluno__grau'].extra['choices'] = [(grau.id, grau) for grau in Grau.objects.all()]
 
     class Meta:
         models = Aluno
-        fields = ['nome', 'sexo', 'estado_civil', 'etnia', 'status__status']
+        fields = ['nome', 'sexo', 'estado_civil', 'etnia', 'status__status', 'probatorio_aluno__grau', 'dt_nascimento']
 
 
 class MatriculaFilter(django_filters.FilterSet):
--- relatorios/views.py
+++ relatorios/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 from matricula.models import Matricula, Probatorio
 from aluno.models import Aluno
 from .filter import MatriculaFilter, AlunoFilter, ProbatorioFilter
-from config.models import Sexo, EstadoCivil, Etnia, StatusOptions
+from config.models import Sexo, EstadoCivil, Etnia, StatusOptions, Grau
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from faker import Factory
 from datetime import datetime
+import csv
+from io import BytesIO
+from django.http import HttpResponse
 
 
 
@@ -34,6 +37,24 @@ def filtra_aluno(request):
     for i in grafico_status:
         i['cor'] = StatusOptions.objects.get(status_options=i['status__status__status_options']).cor
 
+
+    grafico_grau = f.qs.values("probatorio_aluno__grau__grau").annotate(nviews=Count('probatorio_aluno__grau__grau'))
+    for i in grafico_grau:
+        i['cor'] = Grau.objects.get(grau=i['probatorio_aluno__grau__grau']).cor
+
+    grafico_dt_nascimento = f.qs.values("dt_nascimento__year").annotate(nviews=Count('dt_nascimento'))
+
+
+
+    grafico_dt_cadastro = f.qs.values("dt_cadastro__year").annotate(nviews=Count('dt_cadastro'))
+    grafico_mestrado = f.qs.filter(probatorio_aluno__grau__grau='Mestrado').values("dt_cadastro__year").annotate(nviews=Count('dt_cadastro'))
+
+
+
+    grafico_doutorado = f.qs.filter(probatorio_aluno__grau__grau='Doutorado').values("dt_cadastro__year").annotate(nviews=Count('dt_cadastro'))
+    for i in grafico_doutorado:
+        i['cor'] = Grau.objects.get(grau=i['probatorio_aluno__grau__grau']).cor
+
     # TODO GRAFICO DOS ALUNOS QUE ENTRARAM NOS ÚLTIMOS ANOS
     # curr_year = datetime.now().year
     # quadrienio = [curr_year, curr_year-1, curr_year-2, curr_year-3, curr_year-4, curr_year-5, curr_year-6]
@@ -56,4 +77,40 @@ def filtra_aluno(request):
     except EmptyPage:
         response = paginator.get_page(paginator.num_pages)
 
-    return render(request, 'filtra.html', {'filter': f, 'alunos': response, 'grafico_sexo': grafico_sexo, 'grafico_estado_civil': grafico_estado_civil, 'grafico_etnia': grafico_etnia, 'grafico_status': grafico_status})
+    if request.method == 'POST':
+        print(str(request.POST.get('export')))
+        if str(request.POST.get('export')) == 'csv' :
+            return export_cliente_csv(request, f.qs)
+
+
+    return render(request, 'filtra.html', {'filter': f, 'alunos': response, 'grafico_sexo': grafico_sexo, 'grafico_estado_civil': grafico_estado_civil, 'grafico_etnia': grafico_etnia, 'grafico_status': grafico_status, 'grafico_grau': grafico_grau, 'grafico_dt_nascimento': grafico_dt_nascimento, 'grafico_dt_cadastro': grafico_dt_cadastro, 'grafico_mestrado': grafico_mestrado, 'grafico_doutorado': grafico_doutorado})
+
+
+def export_cliente_csv(request, clientes):
+    
+    # Get all data from cDetail Databse Table
+    clientes = clientes.all()
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="alunos.csv"'
+
+    fields = ['Nome', 'Idade', 'CPF', 'Email', 'Sexo', 'Estado Civil', 'Profissão']
+    writer = csv.writer(response)
+    writer.writerow(fields)    
+
+    for c in clientes:
+
+        obj = [c.nome, c.cpf, c.sexo, c.nome_pai, c.nome_mae, c.naturalidade, c.nacionalidade, c.dt_nascimento, c.estado_civil, c.identidade, c.identidade_uf, c.identidade_orgao, c.sexo, c.email, c.etnia, c.portador_deficiencia, c.portador_deficiencia_qual, c.status, c.probatorio_aluno.probatorio, c.probatorio_aluno.data_inscricao, c.probatorio_aluno.linha_pesquisa, c.probatorio_aluno.grau]
+        if(c.probatorio_aluno.matricula_probatorio.first() is not None):
+            obj += [c.probatorio_aluno.matricula_probatorio.first().numero, c.probatorio_aluno.matricula_probatorio.first().curso, c.probatorio_aluno.matricula_probatorio.first().requisita_bolsa, c.probatorio_aluno.matricula_probatorio.first().linha_pesquisa, c.probatorio_aluno.matricula_probatorio.first().grau, c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.titulo, c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.nota, c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.versao_final, c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.dt_diploma, c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.diploma]
+            if(c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.orientacao_trabalho_final is not None):                
+                aux = ''
+                for o in c.probatorio_aluno.matricula_probatorio.first().matricula_trabalho_final.orientacao_trabalho_final.all():
+                    aux += o.professor.nome + ' '
+                    print(aux)
+            obj += [aux]
+                    
+        writer.writerow(obj)
+
+    return response
