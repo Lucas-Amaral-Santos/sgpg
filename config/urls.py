70c2ab965ffcaae438fad0e09e9935a005690277
lucas <amarallucas@id.uff.br>
2024-11-16 01:50:47 -0300

    Atualizar relatório

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import lista_tabelas
+from .views import lista_tabelas, delete_object, edit_object
 
 app_name = 'config'
 
 urlpatterns = [
     path('lista_tabelas/', lista_tabelas, name='lista_tabelas'),
+    path('delete/<str:tabela>/<int:id>', delete_object, name='delete_object'),
+    path('edit/<str:tabela>/<int:id>', edit_object, name='edit_object'),
 
 ]
